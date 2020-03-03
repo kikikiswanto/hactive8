0874bc0bed583443a5ed840634f42107314190ac
Kiki Kiswanto <18055870@BTPN>
2020-03-03 13:12:14 +0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+function MultiplicativePersistence(num) {
+    
+    function multiply(n){
+        return n.reduce(function(a,b){return a*b;});
+    }
+    var count =0; 
+ 
+    while(num.toString().length > 1) {
+        num= num.toString().split("");
+        num = multiply(num);
+        count++;
+     }
+     return count;
+ }
+ 
+console.log(MultiplicativePersistence("39"));
